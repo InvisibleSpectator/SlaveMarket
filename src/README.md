072ac3edfa46fd65a63295272e594fc18b67df61
s3rius <win10@list.ru>
2017-11-13 17:53:37 +0400

    Fixed some git issues.

--- src/README.md
+++
@@ -1,1 +0,0 @@
-# slave Market project
--- src/src/main/java/ru/cracker/exceptions/WrongQueryException.java
+++
@@ -1,8 +0,0 @@
-package ru.cracker.exceptions;
-
-public class WrongQueryException extends IllegalArgumentException {
-    public WrongQueryException(String query){
-        super("Illegal query \"" + query + "\"");
-    }
-
-}
